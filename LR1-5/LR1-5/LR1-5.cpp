07fc967d8fb7ca3630d520a516fb6f37c08dfe0d
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-09-12 16:01:25 +0300

    LR1

---
+++ LR1-5/LR1-5/LR1-5.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    ifstream f;
+    f.open("C:/Users/User/Desktop/input.txt");
+    ofstream f2("C:/Users/User/Desktop/output.txt");
+    if (!f.is_open()) {
+        cout << "Error";
+    }
+    else {
+        cout << "File open" << endl;
+        string str;
+        while (!f.eof()) {
+            str = "";
+            f >> str;
+            reverse(str.begin(), str.end());
+            f2 << str << " ";
+        }
+    }
+    cout << "Completed" << endl;
+    f.close();
+    f2.close();
+    return 0;
+}
+
