09d906af3263555bdc147b608f5f6af7389dc7fe
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-09-19 18:06:02 +0300

    LR2

--- LR1-5/LR1-5/LR1-5.cpp
+++ LR1-5/LR1-5/LR1-5.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+const int N = 30000;
+//1
 int main()
 {
     setlocale(LC_ALL, "rus");
@@ -30,3 +32,61 @@ int main()
     return 0;
 }
 
+
+//2
+int main() {
+    ifstream f;
+    f.open("input.txt");
+    ofstream f2("output.txt");
+    if (!f.is_open()) {
+        cout << "Error";
+    }
+    else {
+        cout << "File open" << endl;
+        int n, m, d ,c;
+        int b[N], k[N];
+
+        while (!f.eof()) {
+            f >> n;
+            f >> m;
+            f >> d;
+            f >> c;
+            for (int i = 0; i < m + 1; ++i) {
+                f >> b[i];
+                if (b[i] > n) {
+                    f2 << "NO";
+                    cout << "Completed";
+                    f.close();
+                    f2.close();
+                    exit(0);
+                }
+            }
+        }
+        for (int j = 0; j < m + 1; ++j) {
+            for (int i = 0; i < n; ++i) {
+                int l = 1;
+                l = (l + i) * d + c;
+                if (l < 13) {
+                    if (l == b[j]) {
+                        k[j] = i + 1;
+                        break;
+                    }
+                }
+                else {
+                    l = l % 13;
+                    if (l == b[j]) {
+                        k[j] = i + 1;
+                        break;
+                    }
+                }
+            }
+        }
+        f2 << "YES" << endl;
+        for (int j = 0; j < m; ++j) {
+            f2 << k[j] << " ";
+        }
+    }
+    cout << "Completed";
+    f.close();
+    f2.close();
+}
