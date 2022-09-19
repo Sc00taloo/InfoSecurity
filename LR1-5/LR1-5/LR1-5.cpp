5dc6a271ec7ddc5b2a0f4d55f905a73ea76b660e
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-09-19 19:12:10 +0300

    LR4

--- LR1-5/LR1-5/LR1-5.cpp
+++ LR1-5/LR1-5/LR1-5.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 const int N = 30000;
+const int N1 = 10000;
 //1
 int main()
 {
@@ -85,6 +86,76 @@ int main() {
         for (int j = 0; j < m; ++j) {
             f2 << k[j] << " ";
         }
+    }
+    cout << "Completed";
+    f.close();
+    f2.close();
+}
+
+
+//3
+int main() {
+    ifstream f;
+    f.open("C:/Users/User/Desktop/input.txt");
+    ofstream f2("C:/Users/User/Desktop/output.txt");
+    if (!f.is_open()) {
+        cout << "Error";
+    }
+    else {
+        cout << "File open" << endl;
+        int n;
+        string s1, s2;
+        while (!f.eof()) {
+            f >> n;
+            f >> s1;
+            f >> s2;
+        }
+
+    }
+    cout << "Completed";
+    f.close();
+    f2.close();
+}
+
+
+//4
+int main() {
+    ifstream f;
+    f.open("input.txt");
+    ofstream f2("output.txt");
+    if (!f.is_open()) {
+        cout << "Error";
+    }
+    else {
+        cout << "File open" << endl;
+        int n, a[N1], b[N1], c[N1];
+        while (!f.eof()) {
+            f >> n;
+            for (int i = 0; i < n; ++i) {
+                f >> a[i];
+                f >> b[i];
+                f >> c[i];
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1 + i; j < n; ++j) {
+                int count = 0;
+                if (a[i] == a[j]) {
+                    count++;
+                }
+                if (b[i] == b[j]) {
+                    count++;
+                }
+                if (c[i] == c[j]) {
+                    count++;
+                }
+                if (count == 1) {
+                    sum++;
+                }
+            }
+        }
+        f2 << sum;
     }
     cout << "Completed";
     f.close();
